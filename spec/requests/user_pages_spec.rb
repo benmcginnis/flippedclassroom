02eaaa32aedacf3b8b9c9f64cad699334e7c7a22
Ben McGinnis <mcginnisb@gmail.com>
2012-09-09 16:47:04 -0400

    Edit page ready

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,7 +19,7 @@ describe "User pages" do
     it { should have_selector('title', text: user.name) }
   end
 
-describe "signup" do
+  describe "signup" do
 
     before { visit signup_path }
 
@@ -57,6 +57,23 @@ describe "signup" do
           it { should have_link('Sign in') }
         end
       end
+    end
+  end
+  
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
     end
   end
 
