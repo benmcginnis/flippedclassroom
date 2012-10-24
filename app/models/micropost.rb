bd214d1810996bbbd897b9090234b0e347fc55a6
Ben McGinnis <mcginnisb@gmail.com>
2012-10-23 20:25:26 -0400

    Ok, gotten to the has_many and belongs_to associations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  attr_accessible :content
   belongs_to :user
 
-  validates :user_id, presence: true  
+  validates :user_id, presence: true
 end
