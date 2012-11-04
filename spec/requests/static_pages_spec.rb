5be73a07439c30b4d24934a9b87eadc5d777e58d
Ben McGinnis <mcginnisb@gmail.com>
2012-11-04 15:45:55 -0500

    Removed Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('h1',    text: 'Flipped Classroom') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
 
@@ -53,25 +53,16 @@ describe "Static pages" do
     it { should have_selector('title', text: full_title('About Us')) }
   end
 
-  describe "Contact page" do
-    before { visit contact_path }
-
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
-  end
-
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
     page.should have_selector 'title', text: full_title('About Us')
     click_link "Help"
     page.should have_selector 'title', text: full_title('Help')
-    click_link "Contact"
-    page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
     click_link "flipped classroom"
-    page.should have_selector 'h1', text: 'Sample App'
+    page.should have_selector 'h1', text: 'Flipped Classroom'
   end
 end
