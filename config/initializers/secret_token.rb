84b3fa060d8bb6f5d1d2a7608f10f22164e3df57
Ben McGinnis <mcginnisb@gmail.com>
2012-11-10 11:50:06 -0500

    Renamed application

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'a9111c4d20f130d3e9f0d497297400fb54dbef3c0bdba5e71b51cc3d564490c014c2a7139998130ef36bea244a59ef6389df9c327c8be67c75d6619b3b067673'
+Flippedclassroom::Application.config.secret_token = 'a9111c4d20f130d3e9f0d497297400fb54dbef3c0bdba5e71b51cc3d564490c014c2a7139998130ef36bea244a59ef6389df9c327c8be67c75d6619b3b067673'
