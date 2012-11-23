dcb3cacf0b0d8e14b5465f2b1590029df66fdec3
Ben McGinnis <bmcginnis@citrrus.com>
2012-11-23 12:05:05 -0500

    Easy navigation between lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,14 @@
 class Lesson < ActiveRecord::Base
   attr_accessible :name
+
+
+  def next_lesson
+  	self.class.first(:conditions => ["id > ?", id], :order => "id asc")
+  end
+
+  def previous_lesson
+  	self.class.first(:conditions => ["id < ?", id], :order => "id desc")
+
+  end
+
 end
